457d901389a208328462f087cf072ec5a287d492
Mose Mueller <mosmuell@ethz.ch>
2022-05-22 10:02:55 +0200

    nvim: ignoring flake8 "line too long" errors.

--- files/users/mose/nvim/lua/user/lsp/null-ls.lua
+++ files/users/mose/nvim/lua/user/lsp/null-ls.lua
@@ -9,11 +9,12 @@ local formatting = null_ls.builtins.formatting
 local diagnostics = null_ls.builtins.diagnostics
 
 null_ls.setup({
-        diagnostics.flake8,
     debug = false,
     sources = {
         formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
         formatting.black.with({ extra_args = { "--fast" } }),
         formatting.stylua,
+        diagnostics.flake8.with({ extra_args = { "--ignore=E501" } }), -- ignoring "line too long"
+        -- diagnostics.cppcheck.with({extra_args = { "--language=cpp" }})
     },
 })
